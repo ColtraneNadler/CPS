b8cd31e141379b6e209b6c86d39544cfc6c8e772
Horrorpenguin <horrorpenguin123@gmail.com>
2015-04-28 11:41:00 -0400

    CPS Test

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use('assets/style.css');
+
+app.get('/', function(req, res) {
+	res.render('pages/home.ejs');
+});
+
+
+
+
+var port = 3000;
+app.listen(port, function() {
+	console.log('Listening on port:' + port + '.');
+})
