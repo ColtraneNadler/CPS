901a394b38617d5e0922ac1277761d4510df2a86
Horrorpenguin <horrorpenguin123@gmail.com>
2015-05-02 23:55:56 -0400

    Added Redis Support

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var fs = require('fs');
-var myJson = {
-	key: "myvalue"
-}
+var redis = require('redis');
+var client = redis.createClient(6379, '****');
+var jquery = require('jquery');
 
-var file = require('./filename.json')
+client.on('connect', function() {
+    console.log('Connected to REDIS');
+})
 
-fs.writeFile('filename.json', JSON.stringify( myJson ), 'utf8')
+
+client.on('error', function() {
+    console.log('Could not establish connection to REDIS')
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +31,13 @@ app.get('/leaderboards', function(req, res) {
 });
 
 
-
 var port = 3000;
 app.listen(port, function() {
 	console.log('Listening on port:' + port + '.');
 })
+
+app.locals
+
+app.locals.saveClicks = function(user, cps) {
+    client.set(user, cps);
+}
